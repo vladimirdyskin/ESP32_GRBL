17c8e023f6b4ca6f62f8baa12f246286aa730f60
vladimirdyskin <vladimirdyskin@me.com>
2021-07-06 15:01:03 +0500

    jjj

--- Grbl_Esp32/src/Machines/3axis_v4.h
+++ Grbl_Esp32/src/Machines/3axis_v4.h
@@ -25,8 +25,10 @@
     You should have received a copy of the GNU General Public License
     along with Grbl_ESP32.  If not, see <http://www.gnu.org/licenses/>.
 */
+#define MACHINE_NAME            "Sunrise3D"
 
-#define MACHINE_NAME            "ESP32_V4"
+#define ENABLE_WIFI
+#define ENABLE_CAPTIVE_PORTAL
 
 #define X_STEP_PIN              GPIO_NUM_12
 #define X_DIRECTION_PIN         GPIO_NUM_14
@@ -42,8 +44,9 @@
 // OK to comment out to use pin for other features
 #define STEPPERS_DISABLE_PIN    GPIO_NUM_13
 
-#define SPINDLE_TYPE SpindleType::NONE
-// #define SPINDLE_OUTPUT_PIN      GPIO_NUM_21   // labeled SpinPWM
+#define SPINDLE_TYPE SpindleType::PWM
+#define SPINDLE_OUTPUT_PIN      GPIO_NUM_21   // labeled SpinPWM
+#define INVERT_SPINDLE_PWM 1
 // #define SPINDLE_ENABLE_PIN      GPIO_NUM_22  // labeled SpinEnbl
 
 //#define COOLANT_MIST_PIN        GPIO_NUM_21  // labeled Mist
@@ -59,48 +62,48 @@
 */
 
 // defaults
-// #define DEFAULT_STEP_PULSE_MICROSECONDS 3
-// #define DEFAULT_STEPPER_IDLE_LOCK_TIME 250 // stay on
+#define DEFAULT_STEP_PULSE_MICROSECONDS 10
+#define DEFAULT_STEPPER_IDLE_LOCK_TIME 255 // stay on
 
-// #define DEFAULT_STEPPING_INVERT_MASK 0 // uint8_t
-// #define DEFAULT_DIRECTION_INVERT_MASK 0 // uint8_t
-// #define DEFAULT_INVERT_ST_ENABLE 0 // boolean
-// #define DEFAULT_INVERT_LIMIT_PINS 1 // boolean
-// #define DEFAULT_INVERT_PROBE_PIN 0 // boolean
+#define DEFAULT_STEPPING_INVERT_MASK 0 // uint8_t
+#define DEFAULT_DIRECTION_INVERT_MASK 5 // uint8_t
+#define DEFAULT_INVERT_ST_ENABLE 0 // boolean
+#define DEFAULT_INVERT_LIMIT_PINS 0 // boolean
+#define DEFAULT_INVERT_PROBE_PIN 0 // boolean
 
 // #define DEFAULT_STATUS_REPORT_MASK 1
 
-// #define DEFAULT_JUNCTION_DEVIATION 0.01 // mm
-// #define DEFAULT_ARC_TOLERANCE 0.002 // mm
+#define DEFAULT_JUNCTION_DEVIATION 0.01 // mm
+#define DEFAULT_ARC_TOLERANCE 0.002 // mm
 // #define DEFAULT_REPORT_INCHES 0 // false
 
-// #define DEFAULT_SOFT_LIMIT_ENABLE 0 // false
-// #define DEFAULT_HARD_LIMIT_ENABLE 0  // false
+#define DEFAULT_SOFT_LIMIT_ENABLE 0 // false
+#define DEFAULT_HARD_LIMIT_ENABLE 1  // false
 
-// #define DEFAULT_HOMING_ENABLE 0
+#define DEFAULT_HOMING_ENABLE 1
 // #define DEFAULT_HOMING_DIR_MASK 0 // move positive dir Z, negative X,Y
-// #define DEFAULT_HOMING_FEED_RATE 200.0 // mm/min
-// #define DEFAULT_HOMING_SEEK_RATE 1000.0 // mm/min
-// #define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
-// #define DEFAULT_HOMING_PULLOFF 3.0 // mm
+#define DEFAULT_HOMING_FEED_RATE 25.0 // mm/min
+#define DEFAULT_HOMING_SEEK_RATE 350.0 // mm/min
+#define DEFAULT_HOMING_DEBOUNCE_DELAY 250 // msec (0-65k)
+#define DEFAULT_HOMING_PULLOFF 4.0 // mm
 
 // #define DEFAULT_SPINDLE_RPM_MAX 1000.0 // rpm
 // #define DEFAULT_SPINDLE_RPM_MIN 0.0 // rpm
 
 // #define DEFAULT_LASER_MODE 0 // false
 
-// #define DEFAULT_X_STEPS_PER_MM 80.0
-// #define DEFAULT_Y_STEPS_PER_MM 80.0
-// #define DEFAULT_Z_STEPS_PER_MM 100.0 // This is percent in servo mode...used for calibration
+#define DEFAULT_X_STEPS_PER_MM 750.0
+#define DEFAULT_Y_STEPS_PER_MM 250.0
+#define DEFAULT_Z_STEPS_PER_MM 3200.0 // This is percent in servo mode...used for calibration
 
-// #define DEFAULT_X_MAX_RATE 5000.0 // mm/min
-// #define DEFAULT_Y_MAX_RATE 5000.0 // mm/min
-// #define DEFAULT_Z_MAX_RATE 5000.0 // mm/min
+#define DEFAULT_X_MAX_RATE 9000.0 // mm/min
+#define DEFAULT_Y_MAX_RATE 7000.0 // mm/min
+#define DEFAULT_Z_MAX_RATE 400.0 // mm/min
 
-// #define DEFAULT_X_ACCELERATION 50.0 // mm/sec^2. 50 mm/sec^2 = 180000 mm/min^2
-// #define DEFAULT_Y_ACCELERATION 50.0 // mm/sec^2
-// #define DEFAULT_Z_ACCELERATION 50.0 // mm/sec^2
+#define DEFAULT_X_ACCELERATION 30.0 // mm/sec^2. 50 mm/sec^2 = 180000 mm/min^2
+#define DEFAULT_Y_ACCELERATION 200.0 // mm/sec^2
+#define DEFAULT_Z_ACCELERATION 10.0 // mm/sec^2
 
-// #define DEFAULT_X_MAX_TRAVEL 300.0 // mm NOTE: Must be a positive value.
-// #define DEFAULT_Y_MAX_TRAVEL 300.0 // mm NOTE: Must be a positive value.
-// #define DEFAULT_Z_MAX_TRAVEL 100.0 // This is percent in servo mode...used for calibration
+#define DEFAULT_X_MAX_TRAVEL 200.0 // mm NOTE: Must be a positive value.
+#define DEFAULT_Y_MAX_TRAVEL 200.0 // mm NOTE: Must be a positive value.
+#define DEFAULT_Z_MAX_TRAVEL 200.0 // This is percent in servo mode...used for calibration
